87a94db33c1d829ac388e23bee46b943f0929dba
jonathan <Jonathan.Heinz@t-systems.com>
2021-12-03 11:08:24 +0100

    Test lint failure

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 def main():
     print("Hello World")
 
+    print("this is a very very long line aaaaaaaaaaaaaaaaaaaaaaaaaxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
 
 if __name__ == '__main__':
     main()
