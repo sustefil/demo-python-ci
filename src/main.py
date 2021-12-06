4cf33c21d4525a8a4258832471f6c197b03686ce
Filip Suster <filip.suster@t-mobile.cz>
2021-12-06 12:15:02 +0100

    Add some linter tests

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
+
 def main():
     print("Hello World")
 
     print("this is a very very long line aaaaaaaaaaaaaaaaaaaaaaaaaxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
+    CamelCase = 'test'
+
+
+def CamelCaseTestAgain():
+    try:
+        x = 1
+        y = x / 0
+    except Exception as e:
+        print('Too broad exception?')
+
+    if True:
+        print('True is True indeed')
+
 
 if __name__ == '__main__':
     main()
+
+
